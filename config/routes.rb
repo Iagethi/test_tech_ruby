7265d81353bc1ddc2846cc6f6a9f420ff1627136
LucaWin <omotob_l@etna-alternance.net>
2021-10-22 22:10:02 +0200

    [FIX] route name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :cours
   resources :formations
-  get '/coursi/:id', to: 'cours#show_by_formation'
+  get '/coursby/:id', to: 'cours#show_by_formation'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
